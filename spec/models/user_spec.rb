233de166d7d608825c95698fa2356b4e17affd9f
Thomas Leskin <tleskin@gmail.com>
2015-04-23 13:45:18 -0600

    Initial commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  let(:user){User.new( username: 'patrick')}
+
+  it 'is valid' do
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a username' do
+    user.username = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'has a default point values of zero' do
+    expect(user.available_points).to be(0)
+    expect(user.redeem_points).to be(0)
+  end
+end
